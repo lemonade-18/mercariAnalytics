7b777f1cc68ce127f996363550ac3c42a85cf323
masayuki kasano <mk1734@mailg.denpa.ac.jp>
2020-01-21 13:42:28 +0900

    変数名の変更

--- graph.py
+++ graph.py
@@ -3,14 +3,15 @@ import math
 
 def graphdata(list):
 
+    # リストを配列に代入
+    price = [y[1] for y in list]
+    sold = [x[2] for x in list]
+
     # スタージェスの公式
     frequency = round(1 + math.log2(len(list)))
 
     data = [0] * frequency
     hierarchy = [0] * frequency
-
-    price = [y[1] for y in list]
-    sold = [x[2] for x in list]
 
     # 最大値と最小値を求める
     max_price = 0
@@ -27,25 +28,22 @@ def graphdata(list):
     # print(max_price)
     # print(sold)
 
-    # 階層の数を求める
+    # 階級幅
     sold_elements = 0
     average = int((max_price - min_price)/frequency/10000)
     sold_count = 0
     average = int((average+1)*10000)
-    # print(average)
-    print(frequency)
-    print(data)
-    print(hierarchy)
+
     for i in range(frequency-1):
         hierarchy[i+1] = hierarchy[i]+average
 
+    # 階級幅毎の件数
     for money in price:
         if sold[sold_elements] == 'sold':
             sold_count += 1
-            dataElements = int(money / average)
-            print(dataElements)
-            if dataElements < frequency:
-                data[dataElements] += 1
+            data_elements = int(money / average)
+            if data_elements < frequency:
+                data[data_elements] += 1
             else:
                 data[frequency - 1] += 1
         sold_elements += 1
@@ -58,27 +56,28 @@ def graphdata(list):
     data_text = data_text[:-1]
     # print(data_text)
 
-    # labelsTextの整形
-    labelsText = ""
+    # labels_textの整形
+    labels_text = ""
     for val in hierarchy:
-        labelsText += "'"
-        labelsText += "\xA5"
-        labelsText += str("{:,d}".format(val))
-        labelsText += "~"
-        labelsText += "'"
-        labelsText += ","
-    labelsText = labelsText[:-1]
-    # print(labelsText)
+        labels_text += "'"
+        labels_text += "\xA5"
+        labels_text += str("{:,d}".format(val))
+        labels_text += "~"
+        labels_text += "'"
+        labels_text += ","
+    labels_text = labels_text[:-1]
+    # print(labels_text)
 
-    # maxsoldnum
-    maxSoldNum = int((max(data)/10 + 1)) * 10
+    # max_sold_num
+    max_sold_num = int((max(data)/10 + 1)) * 10
 
+    # ステップ数
     step_size = 5
 
-    if maxSoldNum >= 100:
+    if max_sold_num >= 100:
         step_size = 10
-    elif maxSoldNum < 30:
+    elif max_sold_num < 30:
         step_size = 1
 
-    result = [data_text, labelsText, maxSoldNum, step_size]
+    result = [data_text, labels_text, max_sold_num, step_size]
     return result
